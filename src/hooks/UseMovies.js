01220a0e5592dd922f8f575727841a5e322def1b
Alexander Perez <alexander.pereze02@gmail.com>
2023-05-29 18:23:17 -0400

    changessss

--- src/hooks/UseMovies.js
+++ src/hooks/UseMovies.js
@@ -1,16 +1,19 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 export function useMovies ({ search }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
+  const antSearch = useRef('')
 
   const getMovies = async () => {
     try {
+      if (search === antSearch.current) return
       setLoading(true)
       setError(null)
       const newMovies = await searchMovies({ search })
+      antSearch.current = search
       setMovies(newMovies)
     } catch (error) {
       setError(error.message)
